922355a0dcb7a45d3740f844dcf66205d0f3ff78
Raf_Jr <rafhael.c.junior@gmail.com>
2021-03-02 21:32:45 -0300

    create the repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles'
 
+import logoImg from '../../assets/logo.svg';
 interface RepositoryParams {
   repository: string;
 }
 
+
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt='Github Explorer' />
+        <Link to='/'>
+          <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src='https://avatars.githubusercontent.com/u/14079111?s=460&u=eaa08c40a5e54f2ab01adbf6a954835156d981d1&v=4' alt='Rafhael' />
+          <div>
+            <strong>Rafhael/Unform</strong>
+            <p>Uma descricao top</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link
+          to={`/repositories/`}
+        >
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 }
 
 export default Repository;
